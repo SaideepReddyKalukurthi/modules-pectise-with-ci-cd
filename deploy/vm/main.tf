a8421fc5ba68a102997b5493c4446bac7000e50b
SaideepReddyKalukurthi <saideepreddy.k2@gmail.com>
2021-12-04 18:27:36 +0530

    first commit

---
+++ deploy/vm/main.tf
@@ -0,0 +1,40 @@
+terraform {
+  required_providers {
+    google = {
+        source = "hashicorp/google"
+    }
+  }
+  backend "gcs" {
+      bucket = "terraform-remote-state-data"
+      prefix = "deploy/vm/state"
+  }
+}
+
+provider "google" {
+    project = var.project-name
+    region = var.region
+}
+
+resource "google_compute_instance" "vm-1" {
+    name = "sai-001"
+    machine_type = "n1-standard-1"
+    zone = "${var.region}-a"
+    network_interface {
+      network = "default"
+
+      access_config {
+        
+      }
+    }
+    boot_disk {
+        initialize_params {
+          image = "debian-cloud/debian-9"
+        }
+    }
+    service_account {
+    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
+    email  = google_service_account.default.email
+    scopes = ["cloud-platform"]
+  }
+  
+}
---
+++ deploy/vm/variables.tf
@@ -0,0 +1,14 @@
+variable "project-name" {
+    type = string
+    default = "terraform-practise-003"
+    description = "(optional) describe your variable"
+}
+
+variable "region" {
+    type = string
+    description = "(optional) describe your variable"
+    default = "us-central1"
+}
+
+
+
