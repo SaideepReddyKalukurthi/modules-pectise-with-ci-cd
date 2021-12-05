c66abf5205421732a00d2a5a6f13721b2c64bfbd
SaideepReddyKalukurthi <saideepreddy.k2@gmail.com>
2021-12-05 14:42:32 +0530

    modules network:vpc,subnets added and deploy network:vpc,subnets added

--- deploy/network/vpc/main.tf
+++ deploy/network/vpc/main.tf
@@ -13,33 +13,3 @@ module "vpc" {
 
     shared_vpc_host = false
 }
-
-module "vpc-1" {
-    source = "../../../modules/network"
-    project_id   = var.project-name
-    network_name = "${var.network-name}-001"
-
-
-    subnets = [
-        {
-            subnet_name           = "group-01"
-            subnet_ip             = "10.10.10.0/24"
-            subnet_region         = "us-west1"
-            subnet_private_access = true
-        },
-        {
-            subnet_name           = "group-02"
-            subnet_ip             = "10.10.20.0/24"
-            subnet_region         = "us-west1"
-            subnet_private_access = true
-        },
-        {
-            subnet_name           = "group-03"
-            subnet_ip             = "10.10.30.0/24"
-            subnet_region         = "us-west1"
-            subnet_private_access = true
-        },
-    ]
-
-    
-}
