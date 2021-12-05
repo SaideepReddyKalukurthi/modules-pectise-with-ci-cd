a0f3972b8f26342c40ee267732c988988f7b7c28
SaideepReddyKalukurthi <saideepreddy.k2@gmail.com>
2021-12-05 18:41:06 +0530

    modules network:cloud-nat added and deploy network:cloud-nat added

--- deploy/network/cloud-nat/main.tf
+++ deploy/network/cloud-nat/main.tf
@@ -6,7 +6,7 @@ module "cloud-nat-group-1" {
   source     = "../../../modules/network/cloud-nat"
   project_id = var.project_id
   region     = var.regions["group-1"]
-  router     = data.terraform_remote_state.network.outputs.router-1
+  router     = google_compute_router.router.name
   name       = "sai-cloud-nat-group-1"
 }
 
@@ -15,7 +15,7 @@ module "cloud-nat-group-2" {
   source     = "../../../modules/network/cloud-nat"
   project_id = var.project_id
   region     = var.regions["group-2"]
-  router     = data.terraform_remote_state.network.outputs.router-2
+  router     = google_compute_router.router.name
   name       = "sai-cloud-nat-group-1"
 }
 
@@ -23,7 +23,7 @@ module "cloud-nat-group-3" {
   source     = "../../../modules/network/cloud-nat"
   project_id = var.project_id
   region     = var.regions["group-3"]
-  router     = data.terraform_remote_state.network.outputs.router-3
+  router     = google_compute_router.router.name
   name       = "sai-cloud-nat-group-1"
 }
 
